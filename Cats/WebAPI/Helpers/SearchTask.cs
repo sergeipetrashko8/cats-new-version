c7d53b141b1fcad40211a4d31e90f1c71674a237
Sergei Petrashko <sergei.petrashko8@gmail.com>
2019-10-21 18:13:39 +0300

    WebAPI: Added Helpers.

---
+++ Cats/WebAPI/Helpers/SearchTask.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+using Quartz;
+
+namespace WebAPI.Helpers
+{
+    public class SearchTask : IJob
+    {
+        public Task Execute(IJobExecutionContext context)
+        {
+            var indecesDirectory = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "App_Data", "Lucene_indeces");
+            if (Directory.Exists(indecesDirectory))
+                Directory.Delete(indecesDirectory, true);
+
+            return Task.CompletedTask;
+        }
+    }
+}
---
+++ Cats/WebAPI/Helpers/TaskScheduler.cs
@@ -0,0 +1,28 @@
+﻿using System.Threading.Tasks;
+using Quartz;
+using Quartz.Impl;
+
+namespace WebAPI.Helpers
+{
+    public class TaskScheduler
+    {
+        public static async Task Start()
+        {
+            var scheduler = await StdSchedulerFactory.GetDefaultScheduler();
+            await scheduler.Start();
+
+            var task = JobBuilder.Create<SearchTask>().Build();
+
+            var trigger = TriggerBuilder.Create()
+                .WithDailyTimeIntervalSchedule
+                (s =>
+                    s.WithIntervalInHours(24)
+                        .OnEveryDay()
+                        .StartingDailyAt(TimeOfDay.HourAndMinuteOfDay(0, 0))
+                )
+                .Build();
+
+            await scheduler.ScheduleJob(task, trigger);
+        }
+    }
+}
