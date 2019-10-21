82dae9032568f67c23b113e1978bbb6e46ca8958
Sergei Petrashko <sergei.petrashko8@gmail.com>
2019-10-21 13:36:16 +0300

    WebAPI: Added bootstrapper.

--- Cats/WebAPI/Startup.cs
+++ Cats/WebAPI/Startup.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -18,12 +19,17 @@ namespace WebAPI
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddControllers();
+            services.AddControllers(config =>
+            { 
+                config.Filters.Add(new ApiControllerAttribute());
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            app.BuildServiceLocation();
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
