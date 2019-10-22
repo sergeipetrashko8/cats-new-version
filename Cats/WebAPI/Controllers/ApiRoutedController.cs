5b7d8af6c229bfbcc27d84c315494f9afbdda4e0
Sergei Petrashko <sergei.petrashko8@gmail.com>
2019-10-22 10:44:31 +0300

    WebAPI: Extended ApiRoutedController.

--- Cats/WebAPI/Controllers/ApiRoutedController.cs
+++ Cats/WebAPI/Controllers/ApiRoutedController.cs
@@ -1,10 +1,20 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System.Net;
+using Microsoft.AspNetCore.Mvc;
 
 namespace WebAPI.Controllers
 {
+    /// <summary>
+    ///     <see cref="ControllerBase"/> class with [<see cref="ApiControllerAttribute"/>] and [<see cref="RouteAttribute"/>("api/[controller]")].
+    /// </summary>
     [ApiController]
     [Route("api/[controller]")]
     public class ApiRoutedController : ControllerBase
     {
+        public ObjectResult ServerError500<T>(T data)
+        {
+            var result = StatusCode((int) HttpStatusCode.InternalServerError, data);
+
+            return result;
+        }
     }
 }
