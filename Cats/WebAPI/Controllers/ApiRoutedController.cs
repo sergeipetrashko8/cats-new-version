340486421c4d85ae6d0adc455bd96fe9817290f4
Sergei Petrashko <sergei.petrashko8@gmail.com>
2019-10-22 20:22:00 +0300

    WebAPi: Extended ApiRoutedController.

--- Cats/WebAPI/Controllers/ApiRoutedController.cs
+++ Cats/WebAPI/Controllers/ApiRoutedController.cs
@@ -10,9 +10,23 @@ namespace WebAPI.Controllers
     [Route("api/[controller]")]
     public class ApiRoutedController : ControllerBase
     {
+        /// <summary>
+        ///     Implements <see cref="HttpStatusCode"/> InternalServerError result.
+        /// </summary>
+        /// <typeparam name="T">Type of data.</typeparam>
+        /// <param name="data">Data object.</param>
+        /// <returns><see cref="object"/> with controller name and error data.</returns>
         public ObjectResult ServerError500<T>(T data)
         {
-            var result = StatusCode((int) HttpStatusCode.InternalServerError, data);
+            var controllerName = this.GetType().Name;
+
+            var response = new
+            {
+                controllerName,
+                data
+            };
+
+            var result = StatusCode((int) HttpStatusCode.InternalServerError, response);
 
             return result;
         }
