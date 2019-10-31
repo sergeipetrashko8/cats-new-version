3bf8c39fd9e5790de48c8740d791850c0641434c
Sergei Petrashko <sergei.petrashko8@gmail.com>
2019-10-31 22:11:34 +0300

    WebAPI: Added some services.

---
+++ Cats/WebAPI/Controllers/Services/Parental/ParentalServiceController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Linq;
+using Application.Core;
+using Application.Infrastructure.GroupManagement;
+using Application.Infrastructure.SubjectManagement;
+using Microsoft.AspNetCore.Mvc;
+using WebAPI.Controllers.Services.Models.Parental;
+
+namespace WebAPI.Controllers.Services.Parental
+{
+    public class ParentalServiceController : ApiRoutedController
+    {
+        private readonly LazyDependency<IGroupManagementService> groupManagementService =
+            new LazyDependency<IGroupManagementService>();
+
+        private readonly LazyDependency<ISubjectManagementService> subjectManagementService =
+            new LazyDependency<ISubjectManagementService>();
+
+        public IGroupManagementService GroupManagementService => groupManagementService.Value;
+
+        public ISubjectManagementService SubjectManagementService => subjectManagementService.Value;
+
+        [HttpGet("GetGroupSubjects/{groupId}")]
+        public IActionResult GetGroupSubjects(string groupId)
+        {
+            try
+            {
+                var group = int.Parse(groupId);
+                var model = SubjectManagementService.GetGroupSubjects(group);
+
+                var result = model.Select(e => new SubjectViewData(e)).ToList();
+
+                return Ok(result);
+            }
+            catch (Exception e)
+            {
+                return ServerError500(e.Message);
+            }
+        }
+    }
+}
---
+++ Cats/WebAPI/Controllers/Services/Practicals/PracticalServiceController.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Json;
+using Application.Core;
+using Application.Infrastructure.SubjectManagement;
+using LMP.Models;
+using Microsoft.AspNetCore.Mvc;
+using WebAPI.Controllers.Services.Models.CoreModels;
+using WebAPI.Controllers.Services.Models.Practicals;
+
+namespace WebAPI.Controllers.Services.Practicals
+{
+    public class PracticalServiceController : ApiRoutedController
+    {
+        private readonly LazyDependency<ISubjectManagementService> subjectManagementService =
+            new LazyDependency<ISubjectManagementService>();
+
+        public ISubjectManagementService SubjectManagementService => subjectManagementService.Value;
+
+        [HttpGet("GetPracticals/{subjectId}")]
+        public IActionResult GetLabs(string subjectId)
+        {
+            try
+            {
+                var sub = SubjectManagementService.GetSubject(int.Parse(subjectId));
+                var model = new List<PracticalsViewData>();
+                if (sub.SubjectModules.Any(e => e.ModuleId == 13))
+                    model = sub.Practicals.Select(e => new PracticalsViewData(e)).ToList();
+
+                return Ok(model);
+            }
+            catch (Exception ex)
+            {
+                return ServerError500(ex.Message);
+            }
+        }
+
+        [HttpPost("Save")]
+        public IActionResult Save(string subjectId, string id, string theme, string duration, string order,
+            string shortName, string pathFile, string attachments)
+        {
+            try
+            {
+                var attachmentsModel = JsonSerializer.Deserialize<List<Attachment>>(attachments).ToList();
+                var subject = int.Parse(subjectId);
+                SubjectManagementService.SavePractical(new Practical
+                {
+                    SubjectId = subject,
+                    Duration = int.Parse(duration),
+                    Theme = theme,
+                    Order = int.Parse(order),
+                    ShortName = shortName,
+                    Attachments = pathFile,
+                    Id = int.Parse(id)
+                }, attachmentsModel, /*todo auth WebSecurity.CurrentUserId*/1);
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                return ServerError500(ex.Message);
+            }
+        }
+
+        [HttpPost("Delete")]
+        public IActionResult Delete(string id, string subjectId)
+        {
+            try
+            {
+                SubjectManagementService.DeletePracticals(int.Parse(id));
+                return Ok();
+            }
+            catch (Exception e)
+            {
+                return ServerError500(e.Message);
+            }
+        }
+
+        [HttpPost("SaveScheduleProtectionDate")]
+        public IActionResult SaveScheduleProtectionDate(string groupId, string date, string subjectId)
+        {
+            try
+            {
+                SubjectManagementService.SaveScheduleProtectionPracticalDate(new ScheduleProtectionPractical
+                {
+                    GroupId = int.Parse(groupId),
+                    Date = DateTime.Parse(date),
+                    SubjectId = int.Parse(subjectId)
+                });
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                return ServerError500(ex.Message);
+            }
+        }
+
+        [HttpPost("SavePracticalsVisitingData")]
+        public IActionResult SavePracticalsVisitingData(List<StudentsViewData> students)
+        {
+            try
+            {
+                foreach (var studentsViewData in students)
+                    SubjectManagementService.SavePracticalVisitingData(studentsViewData.PracticalVisitingMark.Select(
+                        e => new ScheduleProtectionPracticalMark
+                        {
+                            Comment = e.Comment,
+                            Mark = e.Mark,
+                            ScheduleProtectionPracticalId = e.ScheduleProtectionPracticalId,
+                            Id = e.PracticalVisitingMarkId,
+                            StudentId = e.StudentId
+                        }).ToList());
+
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                return ServerError500(ex.Message);
+            }
+        }
+
+        [HttpPost("SaveStudentPracticalsMark")]
+        public IActionResult SaveStudentPracticalsMark(List<StudentsViewData> students)
+        {
+            try
+            {
+                foreach (var studentsViewData in students)
+                    SubjectManagementService.SavePracticalMarks(studentsViewData.StudentPracticalMarks.Select(e =>
+                        new StudentPracticalMark
+                        {
+                            Mark = e.Mark,
+                            PracticalId = e.PracticalId,
+                            Id = e.StudentPracticalMarkId,
+                            StudentId = e.StudentId
+                        }).ToList());
+
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                return ServerError500(ex.Message);
+            }
+        }
+
+        [HttpDelete("DeleteVisitingDate/{id}")]
+        public IActionResult DeleteVisitingDate(string id)
+        {
+            try
+            {
+                SubjectManagementService.DeletePracticalsVisitingDate(int.Parse(id));
+
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                return ServerError500(ex.Message);
+            }
+        }
+    }
+}
